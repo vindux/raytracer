af308e75a6b1de714746a150d200667af5a8f036
leonie.schuberth <leonie.schuberth@hs-duesseldorf.de>
2021-03-08 10:53:19 +0000

    Update SceneObject.java

--- VCG_Raytracer/src/scene/SceneObject.java
+++ VCG_Raytracer/src/scene/SceneObject.java
@@ -5,22 +5,5 @@ import utils.algebra.Vec3;
 public abstract class SceneObject {
 
     public SceneObject() {}
-    /*private Vec3 mPosition = new Vec3();
-
-
-    public SceneObject(Vec3 pos){
-        mPosition = pos;
-    }
-
-    public Vec3 getPosition(){
-        return mPosition;
-    }
-
-    public void setPosition(Vec3 position){
-        mPosition = position;
-    }
-
-    public void moveTo(Vec3 transition){
-        mPosition = mPosition.add( transition );
-    }*/
+   
 }
